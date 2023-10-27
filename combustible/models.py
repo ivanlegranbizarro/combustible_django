f318f889458526339ab4560550b19a005e565901
Iván Legrán <ivanlegranbizarro@gmail.com>
2023-10-27 15:13:27 +0200

    mark model from combustible app

--- combustible/models.py
+++ combustible/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Mark(models.Model):
+    description = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.description
+
+    class Meta:
+        verbose_name = "Marca"
+        verbose_name_plural = "Marcas"
